13481b7d0d998eb5c49d75ccd4e22f51bc46fe08
Shatak-the-imposter <helloshatak@gmail.com>
2024-02-29 21:04:14 +0600

    spninner added only

--- Scripts/phone.js
+++ Scripts/phone.js
@@ -1,18 +1,34 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-    const data = await res.json();
-    const phoneData = data.data
-    console.log(phoneData);
-    displayPhone(phoneData)
+const loadPhone = async (searchText) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+  const data = await res.json();
+  const phoneData = data.data
+  console.log(phoneData);
+  displayPhone(phoneData)
 }
 
 const displayPhone = phones => {
-    const phoneContainer = document.getElementById('phone-container')
-    phones.forEach(phone => {
-        console.log(phone)
-        const phoneCard = document.createElement('div')
-        phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`
-        phoneCard.innerHTML = `
+  const phoneContainer = document.getElementById('phone-container')
+
+  // clear pervious search results from cards container
+  phoneContainer.textContent = '';
+  // display show all button if phone is more than 12
+  const showAllContainer = document.getElementById('show-all-container')
+if (phones.length >12){
+  showAllContainer.classList.remove('hidden')
+}
+else{
+  showAllContainer.classList.add('hidden')
+}
+  // display maximum 12 phones in 1st page
+  phones = phones.slice(0, 12);
+
+
+  // creating cards for each phone dynamicly 
+  phones.forEach(phone => {
+    console.log(phone)
+    const phoneCard = document.createElement('div')
+    phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`
+    phoneCard.innerHTML = `
      <figure><img src="${phone.image}"
      alt="Shoes" /></figure>
      <div class="card-body">
@@ -22,7 +38,14 @@ const displayPhone = phones => {
          <button class="btn btn-primary w-full">See Details</button>
        </div>
      </div>`
-     phoneContainer.appendChild(phoneCard)
-    });
+    phoneContainer.appendChild(phoneCard)
+  });
 }
-loadPhone();
+
+// handelSearchButton
+const handleSearch = () => {
+  const searchBox = document.getElementById('search-box');
+  const searchText = searchBox.value;
+  console.log(searchText)
+  loadPhone(searchText)
+}
